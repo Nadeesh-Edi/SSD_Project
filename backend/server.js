14b79dbf8e73a66ee09d0b5197b268b665c9f8ab
Nisayuru-J <89477809+Nisayuru-J@users.noreply.github.com>
2023-09-30 22:30:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ import connectDB from './config/db.js'
 import cors from 'cors'
 import path from 'path'
 
+import * as helmet from 'helmet';
+import xss from 'xss-clean';
+import * as mongoSanitize from 'express-mongo-sanitize';
+
 // Routes
 import userRoutes from './routes/userRoutes.js'
 import bookingRoutes from './routes/bookingRoutes.js'
@@ -16,6 +20,15 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+// Use helmet middleware to set security headers
+app.use(helmet());
+
+// Use xss-clean middleware to sanitize user input
+app.use(xss());
+
+// Use express-mongo-sanitize middleware to sanitize user input for MongoDB
+app.use(mongoSanitize());
 
 app.use(cors())
 app.use(express.json())
